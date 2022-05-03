e2de32db8b0c1a1d7c46cbda3b303f3d2bf8f873
pamudu-ratnayake <it19993416@my.sliit.lk>
2022-05-03 11:47:09 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+//DB URL
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("        <=== Database connected ! ====>");
+  console.log(`<=== Running on URL: http://localhost:${PORT} ====>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`<=== Server is up and running on port ${PORT} ====>`);
+});
